95ee18c7d27fceb529a85e6453b3e58895bf71e3
Richardzzz <Wen314016548@163.com>
2026-03-06 09:22:15 +0800

    前台搭建

---
+++ heima-leadnews-basic/heima-file-starter/src/main/java/com/heima/file/config/MinIOConfig.java
@@ -0,0 +1,38 @@
+package com.heima.file.config;
+
+import com.heima.file.service.FileStorageService;
+import com.heima.file.service.impl.MinIOFileStorageService;
+import io.minio.MinioClient;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties({ MinIOConfigProperties.class })
+// 当引入FileStorageService接口时
+@ConditionalOnClass(FileStorageService.class)
+@ConditionalOnProperty(prefix = "minio", name = "endpoint")
+public class MinIOConfig {
+
+    @Autowired
+    private MinIOConfigProperties minIOConfigProperties;
+
+    @Bean
+    public MinioClient buildMinioClient() {
+        return MinioClient
+                .builder()
+                .credentials(minIOConfigProperties.getAccessKey(), minIOConfigProperties.getSecretKey())
+                .endpoint(minIOConfigProperties.getEndpoint())
+                .build();
+    }
+
+    @Bean
+    public FileStorageService fileStorageService() {
+        return new MinIOFileStorageService();
+    }
+}
