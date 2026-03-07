5f888a8f40ef0e2b354759c63a3b8182b0b2e6bf
Richardzzz <Wen314016548@163.com>
2026-03-07 11:23:12 +0800

    自动审核实现

--- heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/feign/ArticleClient.java
+++ heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/feign/ArticleClient.java
@@ -3,12 +3,12 @@ package com.heima.article.feign;
 import com.heima.apis.article.IArticleClient;
 import com.heima.article.service.ApArticleService;
 import com.heima.model.article.dtos.ArticleDto;
-import com.heima.model.article.pojos.ApArticle;
+
 import com.heima.model.common.dtos.ResponseResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
