3f8f50bfaeb5720e9c84afc9d2d2c673c5e5f0a4
Richardzzz <Wen314016548@163.com>
2026-03-09 15:14:16 +0800

    延迟审查功能

--- heima-leadnews-service/heima-leadnews-wemedia/src/main/java/com/heima/wemedia/service/impl/WmNewsServiceImpl.java
+++ heima-leadnews-service/heima-leadnews-wemedia/src/main/java/com/heima/wemedia/service/impl/WmNewsServiceImpl.java
@@ -23,6 +23,7 @@ import com.heima.wemedia.mapper.WmNewsMapper;
 import com.heima.wemedia.mapper.WmNewsMaterialMapper;
 import com.heima.wemedia.service.WmNewsAutoScanService;
 import com.heima.wemedia.service.WmNewsService;
+import com.heima.wemedia.service.WmNewsTaskService;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.BeanUtils;
@@ -99,54 +100,57 @@ public class WmNewsServiceImpl extends ServiceImpl<WmNewsMapper, WmNews> impleme
         return responseResult;
     }
 
+    @Autowired
+    private WmNewsTaskService wmNewsTaskService;
+
     /**
      * 发布修改文章或保存为草稿
-     * 
      * @param dto
      * @return
      */
     @Override
     public ResponseResult submitNews(WmNewsDto dto) {
 
-        // 0.条件判断
-        if (dto == null || dto.getContent() == null) {
+        //0.条件判断
+        if(dto == null || dto.getContent() == null){
             return ResponseResult.errorResult(AppHttpCodeEnum.PARAM_INVALID);
         }
 
-        // 1.保存或修改文章
+        //1.保存或修改文章
 
         WmNews wmNews = new WmNews();
-        // 属性拷贝 属性名词和类型相同才能拷贝
-        BeanUtils.copyProperties(dto, wmNews);
-        // 封面图片 list---> string
-        if (dto.getImages() != null && dto.getImages().size() > 0) {
-            // [1dddfsd.jpg,sdlfjldk.jpg]--> 1dddfsd.jpg,sdlfjldk.jpg
+        //属性拷贝 属性名词和类型相同才能拷贝
+        BeanUtils.copyProperties(dto,wmNews);
+        //封面图片  list---> string
+        if(dto.getImages() != null && dto.getImages().size() > 0){
+            //[1dddfsd.jpg,sdlfjldk.jpg]-->   1dddfsd.jpg,sdlfjldk.jpg
             String imageStr = StringUtils.join(dto.getImages(), ",");
             wmNews.setImages(imageStr);
         }
-        // 如果当前封面类型为自动 -1
-        if (WemediaConstants.WM_NEWS_TYPE_AUTO.equals(dto.getType())) {
-            // 设置默认值为无图(0)，避免 mybatis-plus 将 null 忽略导致插入时 MySQL 报“字段 type 无默认值”错误
-            wmNews.setType(WemediaConstants.WM_NEWS_NONE_IMAGE);
+        //如果当前封面类型为自动 -1
+        if(dto.getType().equals(WemediaConstants.WM_NEWS_TYPE_AUTO)){
+            wmNews.setType(null);
         }
 
         saveOrUpdateWmNews(wmNews);
 
-        // 2.判断是否为草稿 如果为草稿结束当前方法
-        if (Short.valueOf(WmNews.Status.NORMAL.getCode()).equals(dto.getStatus())) {
+        //2.判断是否为草稿  如果为草稿结束当前方法
+        if(dto.getStatus().equals(WmNews.Status.NORMAL.getCode())){
             return ResponseResult.okResult(AppHttpCodeEnum.SUCCESS);
         }
 
-        // 3.不是草稿，保存文章内容图片与素材的关系
-        // 获取到文章内容中的图片信息
-        List<String> materials = ectractUrlInfo(dto.getContent());
-        saveRelativeInfoForContent(materials, wmNews.getId());
+        //3.不是草稿，保存文章内容图片与素材的关系
+        //获取到文章内容中的图片信息
+        List<String> materials =  ectractUrlInfo(dto.getContent());
+        saveRelativeInfoForContent(materials,wmNews.getId());
 
-        // 4.不是草稿，保存文章封面图片与素材的关系，如果当前布局是自动，需要匹配封面图片
-        saveRelativeInfoForCover(dto, wmNews, materials);
+        //4.不是草稿，保存文章封面图片与素材的关系，如果当前布局是自动，需要匹配封面图片
+        saveRelativeInfoForCover(dto,wmNews,materials);
 
-        // 审核文章
-        wmNewsAutoScanService.autoScanWmNews(wmNews.getId());
+        //审核文章
+                wmNewsAutoScanService.autoScanWmNews(wmNews.getId());
+      //  wmNewsTaskService.addNewsToTask(wmNews.getId(),wmNews.getPublishTime());
+
         return ResponseResult.okResult(AppHttpCodeEnum.SUCCESS);
 
     }
