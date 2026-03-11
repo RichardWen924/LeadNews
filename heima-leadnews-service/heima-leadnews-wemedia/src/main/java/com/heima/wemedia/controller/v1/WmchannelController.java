7cdb9b0f1e24700f508080fba5001bbc4dfe2777
Richardzzz <Wen314016548@163.com>
2026-03-11 09:14:11 +0800

    admin管理端频道增加功能

--- heima-leadnews-service/heima-leadnews-wemedia/src/main/java/com/heima/wemedia/controller/v1/WmchannelController.java
+++ heima-leadnews-service/heima-leadnews-wemedia/src/main/java/com/heima/wemedia/controller/v1/WmchannelController.java
@@ -1,11 +1,14 @@
 package com.heima.wemedia.controller.v1;
 
+import com.heima.model.admin.pojos.AdChannel;
 import com.heima.model.common.dtos.ResponseResult;
+import com.heima.model.wemedia.pojos.WmChannel;
 import com.heima.wemedia.service.WmChannelService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
 
 @RestController
 @RequestMapping("/api/v1/channel")
@@ -18,5 +21,22 @@ public class WmchannelController {
     @GetMapping("/channels")
     public ResponseResult findAll(){
         return wmChannelService.findAll();
+    }
+
+    @PostMapping("/save")
+    public ResponseResult save(@RequestBody AdChannel channel){
+
+        WmChannel wmChannel = new WmChannel();
+        BeanUtils.copyProperties(channel,wmChannel);
+        if(wmChannel.getIsDefault()==null){
+            wmChannel.setIsDefault(true);
+        }
+        if(wmChannel.getOrd()==null){
+            wmChannel.setOrd(1);
+        }
+        if(wmChannel.getCreatedTime()==null){
+            wmChannel.setCreatedTime(new Date());
+        }
+        return ResponseResult.okResult(wmChannelService.save(wmChannel));
     }
 }
